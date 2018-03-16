07c968d86c96e3eca3e740e62b444f887f1f4d35
StevanMcfc <stevasteva3@gmail.com>
2018-03-16 20:04:47 +0100

    Lista uokivru klase Vulkanizerska radnja prebacena iz private u public. Odradjeno sedam test jUnit metoda za ovu klasu, i uz pomoc njih uklonjene odredjene greske iz metoda za koje su testovi radjeni.

--- VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
+++ VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
@@ -16,7 +16,7 @@ public class VulkanizerskaRadnja {
 	/**
 	 * Lista objekata klase AutoGuma
 	 */
-	private LinkedList<AutoGuma> gume = new LinkedList<AutoGuma>();
+	public LinkedList<AutoGuma> gume = new LinkedList<AutoGuma>();
 	
 	/**
 	 * Klasa koja dodaje gumu u listu, ako objekat nije null, ili vec postoji
@@ -41,11 +41,13 @@ public class VulkanizerskaRadnja {
 		if (markaModel == null)
 			return null;
 		LinkedList<AutoGuma> novaLista = new LinkedList<AutoGuma>();
-		for(int i=0;i<gume.size();i++)
-			if (gume.get(i).equals(markaModel))
+		for(int i=0;i<gume.size();i++) {
+			if (gume.get(i).getMarkaModel().equals(markaModel))
 				novaLista.add(gume.get(i));
+		}
 
-			return novaLista;
+			if(novaLista.size()==0) return null;
+			else return novaLista;
 	}
 	
 }
